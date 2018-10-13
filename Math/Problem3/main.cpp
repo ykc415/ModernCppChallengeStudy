1492f7535d9041e14d23fdc9c698e57f2d961970
ykc415 <pica415@gmail.com>
2018-10-13 18:08:40 +0900

    문제 3번

--- Math/Problem3/main.cpp
+++ Math/Problem3/main.cpp
@@ -1,6 +1,44 @@
 #include <gsl/gsl>
+#include <iostream>
+
+using namespace std;
+
+
+int getGcd(int a, int b) {
+
+    int result = 1;
+
+    if (a > b) {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    for (int i = 1; i <= a; ++i) {
+        if (a % i == 0 && b % i == 0) {
+            result = i;
+        }
+    }
+
+    return result;
+}
 
 int main(int argc, char* argv[])
 {
+    int a, b;
+
+    cin >> a >> b;
+
+    cout << "Input :" << a << "," << b << endl;
+
+    int gcd = getGcd(a, b);
+
+    int div_a = a / gcd;
+    int div_b = b / gcd;
+
+    int lcm = gcd * div_a * div_b;
+
+    cout << "Output :" << lcm << endl;
+
     return 0;
 }
