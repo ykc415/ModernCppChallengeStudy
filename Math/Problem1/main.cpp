1fc248b5a846d6b1a0eae2a422e573390e266b12
ykc415 <pica415@gmail.com>
2018-10-13 17:13:17 +0900

    문제 1번

--- Math/Problem1/main.cpp
+++ Math/Problem1/main.cpp
@@ -1,6 +1,21 @@
 #include <gsl/gsl>
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
+
+    unsigned int limit = 0;
+    std::cout << "Upper limit:";
+    std::cin >> limit;
+
+    unsigned long long sum = 0;
+    for (unsigned int i = 3; i < limit; ++i)
+    {
+        if (i % 3 == 0 || i % 5 == 0)
+            sum += i;
+    }
+
+    std::cout << "sum=" << sum << std::endl;
+
     return 0;
 }
