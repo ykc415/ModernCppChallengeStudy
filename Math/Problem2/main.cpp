a5156cae8d409db6cb8ba97b3d4030a0939c6b03
ykc415 <pica415@gmail.com>
2018-10-13 17:48:50 +0900

    문제2번

--- Math/Problem2/main.cpp
+++ Math/Problem2/main.cpp
@@ -1,6 +1,33 @@
 #include <gsl/gsl>
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char* argv[])
 {
+    int a;
+    int b;
+
+    int result = 1;
+
+    cin >> a;
+    cin >> b;
+
+    cout << "Input : " << a << ", " << b << endl;
+
+    if (a > b) {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    for (int i = 1; i <= a; ++i) {
+        if (a % i == 0 && b % i == 0) {
+            result = i;
+        }
+    }
+
+    cout << "Output : " << result << endl;
+
     return 0;
 }
