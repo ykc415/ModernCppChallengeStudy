8513eb589db3cd0b9fde3c4c388447d9d857312e
ykc415 <pica415@gmail.com>
2018-10-16 18:17:00 +0900

    문제 9번

--- Math/Problem8/main.cpp
+++ Math/Problem8/main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 
 /**
+ * Write a program that prints all Armstrong numbers with three digits.
+ *
  *  An Armstrong number of three digits is an integer such that the sum of the cubes of its digits is equal to
  *  the number itself. For example, 371 is an Armstrong number since 3**3 + 7**3 + 1**3 = 371.
  *  27 + 49 * 7 + 1 = 371
--- Math/Problem9/main.cpp
+++ Math/Problem9/main.cpp
@@ -1,6 +1,46 @@
 #include <gsl/gsl>
+#include <iostream>
+#include <cmath>
+#include <set>
+#include <vector>
+
+/**
+ *  Write a program that prints the prime factors of a number entered by the user.
+ *
+ *
+ */
+
+using namespace std;
+
+void getFactor(int num, vector<int>& store) {
+    for (int i = 2; i <= sqrt(num) ; ++i) {
+        if (num % i == 0) {
+            store.push_back(i);
+            getFactor(num / i, store);
+            return;
+        }
+    }
+
+    store.push_back(num);
+
+}
 
 int main(int argc, char* argv[])
 {
+    vector<int> result;
+
+    int limit;
+
+    cout << "Input : ";
+    cin >> limit;
+
+    getFactor(limit, result);
+
+    for (auto i : result) {
+        cout << i << endl;
+    }
+
     return 0;
 }
+
+
