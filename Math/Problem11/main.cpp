8d31eafab16be22403cdab70aba8106a1fd0969c
ykc415 <pica415@gmail.com>
2018-10-19 17:45:32 +0900

    문제 11번

--- Math/Problem11/main.cpp
+++ Math/Problem11/main.cpp
@@ -1,6 +1,50 @@
 #include <gsl/gsl>
+#include <iostream>
+#include <string>
+#include <vector>
 
-int main(int argc, char* argv[])
+/**
+ * Write a program that, given a number entered by the user, prints its Roman numeral equivalent.
+ *
+ */
+
+
+static std::vector<std::pair<unsigned int, char const*>> roman
+        {
+                { 1000, "M" },{ 900, "CM" },
+                { 500, "D" },{ 400, "CD" },
+                { 100, "C" },{ 90, "XC" },
+                { 50, "L" },{ 40, "XL" },
+                { 10, "X" },{ 9, "IX" },
+                { 5, "V" },{ 4, "IV" },
+                { 1, "I" }
+        };
+
+std::string to_roman(unsigned int value)
 {
-    return 0;
+    std::string result;
+    for (auto const & kvp : roman)
+    {
+        while (value >= kvp.first)
+        {
+            result += kvp.second;
+            value -= kvp.first;
+        }
+    }
+
+    return result;
+}
+
+int main()
+{
+    for(int i = 1; i <= 100; ++i)
+    {
+        std::cout << i << "\t" << to_roman(i) << std::endl;
+    }
+
+    int number = 0;
+    std::cout << "number:";
+    std::cin >> number;
+
+    std::cout << to_roman(number) << std::endl;
 }
