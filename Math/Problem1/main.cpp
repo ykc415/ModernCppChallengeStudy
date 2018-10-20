c9b8cce259f05a02a4d6e026db07ff269012cf5b
ykc415 <pica415@gmail.com>
2018-10-20 12:01:31 +0900

    문제 14번

--- Math/Problem1/main.cpp
+++ Math/Problem1/main.cpp
@@ -9,6 +9,7 @@ int main(int argc, char* argv[])
     std::cin >> limit;
 
     unsigned long long sum = 0;
+
     for (unsigned int i = 3; i < limit; ++i)
     {
         if (i % 3 == 0 || i % 5 == 0)
