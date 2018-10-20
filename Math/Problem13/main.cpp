308025ca85083a9c6dfff5851c37b3e5a33a2e9a
ykc415 <pica415@gmail.com>
2018-10-20 10:27:15 +0900

    문제 13번

--- Math/Problem13/main.cpp
+++ Math/Problem13/main.cpp
@@ -1,6 +1,23 @@
 #include <gsl/gsl>
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+
+
+using namespace std;
+
+float getPI() {
+    float sum = 0;
+
+    for (int n = 0 ; n < 1000000 ; n++) {
+        sum += (pow(-1, n) / (2*n + 1));
+        cout << fixed << setprecision (10) << sum * 4 << endl;
+    }
+}
 
 int main(int argc, char* argv[])
 {
+
+    getPI();
     return 0;
 }
